a599d1ba3c6464bad97ce3031b80f30eec7d4343
Rohan Bhande <rohanbhande11@gmail.com>
2025-04-14 13:57:25 -0500

    redesigned home page

--- lib/core/features/authentication/presentation/login_page.dart
+++ lib/core/features/authentication/presentation/login_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:uni_quest_project/core/constants/app_font_size.dart';
 import 'package:uni_quest_project/core/features/authentication/presentation/register_page.dart';
 import 'package:uni_quest_project/core/features/services/api_service.dart';
 import 'package:uni_quest_project/l10n/app_localizations.dart';
+import '../../../constants/app_colors.dart';
 import '../../../constants/routes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -46,111 +48,123 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
-
+    // 4 looks nice
+    // 7 is okayish
+    // 8 is better than 7
+    // 10 needs to be adjusted
+    // 11 aesthetically look nice tbh
+    // 13 is better
     return Scaffold(
-      // backgroundColor: AppColors.darkYellow,
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/${listOfImages[13]}"),
-              // 4 looks nice
-              // 7 is okayish
-              // 8 is better than 7
-              // 10 needs to be adjusted
-              // 11 aesthetically look nice tbh
-              // 13 is better
-              fit: BoxFit.fill),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Text(
-              //   AppLocalizations.of(context).yourJourneyToFindPerfectUniversity,
-              //   style: const TextStyle(
-              //       fontSize: 16,
-              //       fontWeight: FontWeight.bold,
-              //       color: Colors.white),
-              //   textAlign: TextAlign.center,
-              // ),
-              // Image.asset("assets/images/splash_screen.png"),
-              SizedBox(height: height / 25),
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(
+      backgroundColor: AppColors.darkYellow,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Text(
+            //   AppLocalizations.of(context).yourJourneyToFindPerfectUniversity,
+            //   style: const TextStyle(
+            //       fontSize: 16,
+            //       fontWeight: FontWeight.bold,
+            //       color: Colors.white),
+            //   textAlign: TextAlign.center,
+            // ),
+            // Image.asset("assets/images/splash_screen.png"),
+            Image.asset(
+              "assets/images/app_logo_2.png",
+            ),
+            SizedBox(height: height / 25),
+            Column(
+              children: [
+                Text(
+                  AppLocalizations.of(context).welcomeBack,
+                  style: TextStyle(
+                      fontSize: height * AppFontSize.m, color: Colors.black),
+                ),
+                Text(
+                  AppLocalizations.of(context).loginToYourAccount,
+                  style: TextStyle(
+                    fontSize: height * AppFontSize.xxs,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: height / 25),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
                   fillColor: Colors.white,
                   filled: true,
                   labelText: AppLocalizations.of(context).email,
                   border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
+                      borderRadius: BorderRadius.circular(10))),
+            ),
+            SizedBox(height: height / 40),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                labelText: AppLocalizations.of(context).password,
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
               ),
-              SizedBox(height: height / 40),
-              TextField(
-                controller: passwordController,
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  labelText: AppLocalizations.of(context).password,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                obscureText: true,
+              obscureText: true,
+            ),
+            SizedBox(height: height / 40),
+            GestureDetector(
+              onTap: () {
+                // Handle forgot password
+              },
+              child: Text(
+                AppLocalizations.of(context).forgotPassword,
+                style: TextStyle(color: Colors.blue),
               ),
-              SizedBox(height: height / 40),
-              GestureDetector(
-                onTap: () {
-                  // Handle forgot password
-                },
-                child: Text(
-                  AppLocalizations.of(context).forgotPassword,
-                  style: TextStyle(color: Colors.blue),
-                ),
-              ),
-              SizedBox(height: height / 40),
-              ElevatedButton(
-                onPressed: () async {
-                  final email = emailController.text;
-                  final password = passwordController.text;
-                  print("username : $email");
-                  print("password : $password");
-                  final result = await _apiService.loginStudent(
-                      email: email, password: password);
-                  print("result : $result");
-                  if (result) {
-                    // Save login credentials to SharedPreferences
-                    final prefs = await SharedPreferences.getInstance();
-                    await prefs.setString('email', email);
-                    await prefs.setString('password', password);
+            ),
+            SizedBox(height: height / 40),
+            ElevatedButton(
+              onPressed: () async {
+                final email = emailController.text;
+                final password = passwordController.text;
+                print("username : $email");
+                print("password : $password");
+                final result = await _apiService.loginStudent(
+                    email: email, password: password);
+                print("result : $result");
+                if (result) {
+                  // Save login credentials to SharedPreferences
+                  final prefs = await SharedPreferences.getInstance();
+                  await prefs.setString('email', email);
+                  await prefs.setString('password', password);
 
-                    // Redirect to home page
-                    context.goNamed(RouteNames.homePage);
-                  } else {
-                    // Show error message (e.g., using a snackbar)
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                          content:
-                              Text(AppLocalizations.of(context).loginFailed)),
-                    );
-                  }
-                },
-                child: Text(AppLocalizations.of(context).login),
-              ),
-              SizedBox(height: height / 80),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            const RegisterPage()), // Directly pushing the page widget
+                  // Redirect to home page
+                  context.goNamed(RouteNames.homePage);
+                } else {
+                  // Show error message (e.g., using a snackbar)
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                        content:
+                            Text(AppLocalizations.of(context).loginFailed)),
                   );
-                },
-                child: Text(AppLocalizations.of(context).signUp),
-              ),
-            ],
-          ),
+                }
+              },
+              child: Text(AppLocalizations.of(context).login),
+            ),
+            SizedBox(height: height / 80),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const RegisterPage()), // Directly pushing the page widget
+                );
+              },
+              child: Text(AppLocalizations.of(context).signUp),
+            ),
+          ],
         ),
       ),
     );
